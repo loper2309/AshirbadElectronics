688fb97fd368e8d1331015ad3f49ed50c5f07c12
loper2309 <116813364+loper2309@users.noreply.github.com>
2023-07-05 13:58:18 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+service-worker.const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  './index.html',
+  './style.css',
+  './app.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
